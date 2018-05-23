df7c954049b4791661bb8dc97c856af4b7f081c5
rick <aftchip@gmail.com>
2018-05-22 21:10:24 -0700

    Take Home Assignment completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,5 @@
 var myName = "Rick";
 console.log(myName);
+
+var myHobbiesAndMission = "The main thing I want to get out of this class, is the know how of js, I want to gain access to every potential ability javascript has to offer. One hobby that I enjoy doing is being an avid gamer, crrently I am stuck on fortnite, and today State of Decay 2 came out so I decided to do my hw before I get hooked"
+console.log(myHobbiesAndMission)
