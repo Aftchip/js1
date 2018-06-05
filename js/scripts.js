5b15843be4f13d8ec7073890292cb43d85d17985
rick <aftchip@gmail.com>
2018-06-04 20:33:22 -0700

    day 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,165 +1,27 @@
-//  DAY 4
-// TERNAIRIES
-
-// let inside = true
-
-// if(inside) {
-//   console.log('i am inside')
-// }else {
-//   console.log('i am not inside')
-// }
-// condition ?  expression1 : expresion2
-
-// let example2 = inside ? "i am inside" : "ia m not inside"
-// console.log(example2)
-
-// let number = 15;
-//
-// console.log(number > 10 ? "number is greater than" : "number is less than")
-
-// let name = false;
-// let email = false;
-
-// if(name) {
-//   alert("i have name")
-// }
-// else if (email) {
-//   alert("i have email")
-// }
-// else {
-//   alert("i have none")
-// }
-
-// name ? "i have name" : email ? "i have email" : "i have none"
-// console.log(name ? "i have name" : email ? "i have email" : "i have none")
+// Day5
+//FUNCTIONS
+// console.log("Hello world");
+// array.pop()
 
 
-// let iAmHungry = true;
-// let money = 5;
-//
-// let hungry = iAmHungry ? ((money > 10)? "i am hungry an i have more than 10" : " i am hungry and i have less than 10" ) : ((money > 10)? "i am not hungry and i have more than 10" : "i am not hungry and i have less than 10")
-//
-// console.log(hungry);
-// if (iAmHungry) {
-//   of (money > 10) {
-//     'i am hungry and i have more than 10'
-//   } else {
-//     'i am hungry and i have less than 10'
-//   } else {
-//     if (money > 10)
-//     'i am not hungry and i have more thann 10'
-//   } else {
-//     'i am not hungry and i have less than 10'
-//   }
-// }
+function capitalCase(string) { // FUNCTION DEFENITION, No value until you call.
+  let words = string.split(' ');
+  words.forEach(word => {
+    let letters = word.split('');
+    let capped = letters[0].toUpperCase();
+    letters.splice(0, 1, capped);
+  });
+  return words.join(' ');
+};
+
+let newString = capitalCase("peter piper"); //function CALL.
+
+console.log(newString);
+function(stuff) { //anonymous functon
+  console.log(stuff)
+}
 
 
-
-// let iAmWeraingYellow = false;
-//
-// iAmWeraingYellow ? "yes i am wearing yellow" : "no i am not"
-//
-// let number = 23;
-//
-// let num = (number = 23 || iAmWeraingYellow) ? "number = 23 and/or yellow" : "number not 23 and not yellow"
-// console.log(num)
-
-
-
-
-
-//
-// Day 4
-// ARRAYS []
-
-// let arr = ["javascript", 100, {}, [], true];
-//
-// console.log(arr[0]);
-
-// On your day4 branch, declare a var and define it with an array containing:
-// Your favorite movie of 2017
-// Your lucky number
-// Whether or not you are a Star Wars fan (using a Boolean)
-// The year you graduated high school
-// console.log the var and the length of the array.
-
-// TRY OUT #1
-// let me = ["John Wick 2", 21, true, "Class of 2010"];
-// console.log(me[me.length -1]);
-// console.log(me[3])
-// console.log(me[2])
-//
-//
-// me.push("Pizza");
-// console.log(me);
-// me.pop();
-// console.log(me);
-
-// me.splice(2);//one argument will remove that index and everything after
-// me.splice(2, 1) //with 2 arguments you start removing items at the first argumnt (index) and then remove only the number of items that you passed in as the second argument (delete count).
-// me.splice(2, 1, false)
-// console.log(me, "this is the result of the splice");
-
-
-// let products = [["blanket", [12.99, 10.99], "the best blanket ever"], ['rattle', 4.99, "it rattles man..."], ['diapers', 12.99, "they catch the doo doo"]];
-// alert(products[0][1][1]);
-
-
-
-// In your scripts.js file, create 5 arrays, each with one of your top 5 movies' title and the main character's name
-//
-// Add all of these arrays to a final array named favoriteMovies
-// Create an array with just the titles of your favorite movies and alert it
-// Create another array with your favorite and least favorite of the top 5 and console.log it.
-// Check the length of your favoriteMovies array by alerting it.
-
-
-
-// let favMovie = ['MIB', "J"];
-// let secondFav = ['John Wick', "John"];
-// let thirdFav = ['The good, the bad and the ugly', "Blondie"];
-// let fourthFav = ['Pulp Fiction', "Vincent and Jules"];
-// let fifthFav = ['Dragon', "Liu"];
-//
-// let favoriteMovies = [];
-// favoriteMovies.push(favMovie, secondFav, thirdFav, fourthFav, fifthFav);
-//
-// let titles = [];
-// titles.push(favoriteMovies[0][0], favoriteMovies[1][0], );
-
-
-// LOOPS
-let products = [["blanket", [12.99, 10.99], "the best blanket ever"], ['rattle', 4.99, "it rattles man..."], ['diapers', 12.99, "they catch the doo doo"]];
-
-// let i = 0; //control statement
-
-//while loop
-// while (i < products.length) { //parmeters
-  // console.log(products[i]);
-  // i++; //incrementor
-// }
-
-//for loop
-// for (let i = products.lenght -1; i >= 0; i--) {
-//   console.log(products[i])
-// }
-
-// do while loop
-// let i = 0;
-// let newProduct = ["queeky toy", 19.99, "really good"];
-// do {
-// //add newProduct to the second number of products
-//   if(i === 1) products[i].push(newProduct);
-//   i++;
-// } while (i < products.lenght);
-// console.log(products);
-
-// // for each LOOPS
-// products.foreach((product) => {
-//   console.log(product)
-// })
-//
-//
-
-
-//
+let myCoolFunction = (stuff) => { //SIMPLEST fat arrow functon
+  console.log(stuff)
+};
